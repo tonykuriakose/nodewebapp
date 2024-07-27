67e5d61f0c48b0206b5509f7e28ffb6ac228120b
Tony Kuriakose <t4tonykuriakose@gmail.com>
2024-07-27 22:23:51 +0530

    cuustomer management

--- models/userSchema.js
+++ models/userSchema.js
@@ -21,7 +21,7 @@ const userSchema = new Schema({
     },
     googleId: {
         type : String,
-        unique:true
+        unique:true,
     },
     password : {
         type:String,
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin/adminController");
+const customerController = require("../controllers/admin/customerController");
 const {userAuth,adminAuth} = require("../middlewares/auth");
 
 router.get("/pageerror",adminController.pageerror);
-// Admin login
+// Login Management
 router.get("/login",adminController.loadLogin);
 router.post("/login",adminController.login);
 router.get("/",adminAuth,adminController.loadDashboard);
 router.get("/logout",adminController.logout);
+// Cumstomer Management
+router.get("/users",adminAuth,customerController.customerInfo);
+router.get("/blockCustomer",adminAuth,customerController.customerBlocked);
+router.get("/unblockCustomer",adminAuth,customerController.customerunBlocked);
+
+
+
+
+
+
+
+
 
 
 
