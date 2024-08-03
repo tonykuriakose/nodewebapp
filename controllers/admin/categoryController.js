c3e9ed31a134848be0254f568025bbf79e688ba3
Tony Kuriakose <t4tonykuriakose@gmail.com>
2024-08-04 00:40:25 +0530

    edit category

--- controllers/admin/categoryController.js
+++ controllers/admin/categoryController.js
@@ -111,30 +111,71 @@ const removeCategoryOffer = async (req, res) => {
   }
 };
 
-const getListCategory = async (req,res)=>{
+const getListCategory = async (req, res) => {
   try {
     let id = req.query.id;
-    await Category.updateOne({_id:id},{$set:{isListed:false}});
+    await Category.updateOne({ _id: id }, { $set: { isListed: false } });
     res.redirect("/admin/category");
-    
   } catch (error) {
     res.redirect("/pageerror");
+  }
+};
+
+const getUnlistCategory = async (req, res) => {
+  try {
+    let id = req.query.id;
+    await Category.updateOne({ _id: id }, { $set: { isListed: true } });
+    res.redirect("/admin/category");
+  } catch (error) {
+    res.redirect("/pageerror");
+  }
+};
+
+const getEditCategory = async (req, res) => {
+  try {
+
+    const id = req.query.id;
+    const category = await Category.findOne({_id:id});
+    res.render("edit-category",{category:category});
+    
+  } catch (error) {
+
+    res.redirect("/pageerror")
+    
+  }
+};
+
+
+
+const editCategory = async (req,res)=>{
+  try {
+
+    const id = req.params.id;
+    const {categoryName,description} = req.body;
+    const existingCategory = await Category.findOne({name:categoryName});
+
+    if(existingCategory){
+      return res.status(400).json({error:"Category exists, please choose another name"})
+    }
+
+    const updateCategory =await Category.findByIdAndUpdate(id,{
+      name:categoryName,
+      description:description,
+    },{new:true});
+
+    if(updateCategory){
+      res.redirect("/admin/category");
+    }else {
+      res.status(404).json({error:"Category not found"})
+    }
+
+
+    
+  } catch (error) {
+    res.status(500).json({error:"Internal server error"})
     
   }
 }
-
-const getUnlistCategory = async (req,res)=>{
-  try {
-    let id = req.query.id;
-    await Category.updateOne({_id:id},{$set:{isListed:true}});
-    res.redirect("/admin/category");
-    
-  } catch (error) {
-    res.redirect("/pageerror");
-    
-  }
-}
-
 
 module.exports = {
   categoryInfo,
@@ -143,4 +184,6 @@ module.exports = {
   removeCategoryOffer,
   getListCategory,
   getUnlistCategory,
+  getEditCategory,
+  editCategory,
 };
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -22,6 +22,8 @@ router.post("/addCategoryOffer",adminAuth,categoryController.addCategoryOffer);
 router.post("/removeCategoryOffer",adminAuth,categoryController.removeCategoryOffer);
 router.get("/listCategory",adminAuth,categoryController.getListCategory);
 router.get("/unlistCategory",adminAuth,categoryController.getUnlistCategory);
+router.get("/editCategory",adminAuth,categoryController.getEditCategory);
+router.post("/editCategory/:id",adminAuth,categoryController.editCategory);
 
 
 
