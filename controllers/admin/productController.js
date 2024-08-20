c6c3e23e6e04ecdfdfe142bf3f46c3f4bddd599a
Tony Kuriakose <t4tonykuriakose@gmail.com>
2024-08-20 13:28:33 +0530

    block product

--- controllers/admin/productController.js
+++ controllers/admin/productController.js
@@ -19,157 +19,178 @@ const getProductAddPage = async (req, res) => {
   }
 };
 
-const addProducts = async (req,res)=>{
+const addProducts = async (req, res) => {
   try {
-
     const products = req.body;
     const productExists = await Product.findOne({
-      productName:products.productName,
-
+      productName: products.productName,
     });
 
-    if(!productExists){
+    if (!productExists) {
       const images = [];
 
-      if(req.files && req.files.length>0){
-        for(let i=0;i<req.files.length;i++){
+      if (req.files && req.files.length > 0) {
+        for (let i = 0; i < req.files.length; i++) {
           const originalImagePath = req.files[i].path;
 
-          const resizedImagePath =path.join('public','uploads','product-images',req.files[i].filename);
-          await sharp(originalImagePath).resize({width:440,height:440}).toFile(resizedImagePath);
+          const resizedImagePath = path.join(
+            "public",
+            "uploads",
+            "product-images",
+            req.files[i].filename
+          );
+          await sharp(originalImagePath)
+            .resize({ width: 440, height: 440 })
+            .toFile(resizedImagePath);
           images.push(req.files[i].filename);
         }
       }
 
-      const categoryId = await Category.findOne({name:products.category});
+      const categoryId = await Category.findOne({ name: products.category });
 
-      if(!categoryId){
-        return res.status(400).join("Invalid category name")
+      if (!categoryId) {
+        return res.status(400).join("Invalid category name");
       }
 
       const newProduct = new Product({
-        productName:products.productName,
-        description:products.description,
-        brand:products.brand,
-        category:categoryId._id,
-        regularPrice:products.regularPrice,
-        salePrice:products.salePrice,
-        createdOn:new Date(),
-        quantity:products.quantity,
-        size:products.size,
-        color:products.color,
-        productImage:images,
-        status:'Available',
-       
-
-
+        productName: products.productName,
+        description: products.description,
+        brand: products.brand,
+        category: categoryId._id,
+        regularPrice: products.regularPrice,
+        salePrice: products.salePrice,
+        createdOn: new Date(),
+        quantity: products.quantity,
+        size: products.size,
+        color: products.color,
+        productImage: images,
+        status: "Available",
       });
 
       await newProduct.save();
       return res.redirect("/admin/addProducts");
-
-    }else {
-      return res.status(400).json("Product already exist,please try with another name");
+    } else {
+      return res
+        .status(400)
+        .json("Product already exist,please try with another name");
     }
-    
   } catch (error) {
-    console.error("Error saving product",error);
-    return res.redirect("/admin/pageerror")
-    
+    console.error("Error saving product", error);
+    return res.redirect("/admin/pageerror");
   }
-}
+};
 
 const getAllProducts = async (req, res) => {
   try {
     const search = req.query.search || "";
     const page = req.query.page || 1;
     const limit = 4;
- 
- 
+
     const productData = await Product.find({
       $or: [
         { productName: { $regex: new RegExp(".*" + search + ".*", "i") } },
         { brand: { $regex: new RegExp(".*" + search + ".*", "i") } },
       ],
     })
-    .limit(limit * 1)
-    .skip((page - 1) * limit)
-    .populate('category')
-    .exec();
-
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .populate("category")
+      .exec();
 
     const count = await Product.find({
-      $or:[
-        {productName:{$regex:new RegExp(".*"+search+".*","i")}},
-        {brand:{$regex:new RegExp(".*"+search+".*","i")}},
+      $or: [
+        { productName: { $regex: new RegExp(".*" + search + ".*", "i") } },
+        { brand: { $regex: new RegExp(".*" + search + ".*", "i") } },
       ],
     }).countDocuments();
 
-    const category = await Category.find({isListed:true});
-    const brand = await Brand.find({isBlocked:false});
+    const category = await Category.find({ isListed: true });
+    const brand = await Brand.find({ isBlocked: false });
 
-    if(category && brand){
-      res.render("products",{
-        data:productData,
-        currentPage:page,
-        totalPages:Math.ceil(count/limit),
-        cat:category,
-        brand:brand,
-
-
-      })
-    }else {
+    if (category && brand) {
+      res.render("products", {
+        data: productData,
+        currentPage: page,
+        totalPages: Math.ceil(count / limit),
+        cat: category,
+        brand: brand,
+      });
+    } else {
       res.render("page-404");
     }
- 
   } catch (error) {
-
     res.redirect("/pageerror");
-  
   }
- };
+};
 
- const addProductOffer = async (req, res) => {
+const addProductOffer = async (req, res) => {
   try {
-      const { productId, percentage } = req.body;
-      const findProduct = await Product.findOne({ _id: productId });
-      const findCategory = await Category.findOne({ _id: findProduct.category });
-      if (findCategory.categoryOffer > percentage) {
-          return res.json({ status: false, message: "This product's category already has a category offer." });
-      }
-      findProduct.salePrice = findProduct.salePrice - Math.floor(findProduct.regularPrice * (percentage / 100));
-      findProduct.productOffer = parseInt(percentage);
-      await findProduct.save();
-      findCategory.categoryOffer = 0;
-      await findCategory.save();
-      res.json({ status: true });
+    const { productId, percentage } = req.body;
+    const findProduct = await Product.findOne({ _id: productId });
+    const findCategory = await Category.findOne({ _id: findProduct.category });
+    if (findCategory.categoryOffer > percentage) {
+      return res.json({
+        status: false,
+        message: "This products category already has a category offer",
+      });
+    }
+
+    findProduct.salePrice =
+      findProduct.salePrice -
+      Math.floor(findProduct.regularPrice * (percentage / 100));
+    findProduct.productOffer = parseInt(percentage);
+    await findProduct.save();
+    findCategory.categoryOffer = 0;
+    await findCategory.save();
+    res.json({ status: true });
   } catch (error) {
-      res.redirect("/pageerror");
-      res.status(500).json({ status: false, message: "Internal Server Error" });
+    res.redirect("/pageerror");
+    res.status(500).json({ status: false, message: "Internal Server Error" });
+  }
+};
+
+const removeProductOffer = async (req,res)=>{
+  try {
+
+    const {productId} = req.body
+    const findProduct = await Product.findOne({_id:productId});
+    const percentage = findProduct.productOffer;
+    findProduct.salePrice = findProduct.salePrice+Math.floor(findProduct.regularPrice*(percentage/100));
+    findProduct.productOffer = 0;
+    await findProduct.save();
+    res.json({status:true})
+    
+  } catch (error) {
+    res.redirect("/pageerror")
+    
   }
 }
 
-const removeProductOffer = async (req, res) => {
+const blockProduct = async (req,res)=>{
   try {
-      const {productId} = req.body
-      const findProduct = await Product.findOne({_id : productId})
-      const percentage = findProduct.productOffer
-      findProduct.salePrice = findProduct.salePrice + Math.floor(findProduct.regularPrice * (percentage / 100))
-      findProduct.productOffer = 0
-      await findProduct.save()
-      res.json({status : true})
+
+    let id = req.query.id;
+    await Product.updateOne({_id:id},{$set:{isBlocked:true}});
+    res.redirect("/admin/products");
+    
   } catch (error) {
-      res.redirect("/pageerror");
-     
+    res.redirect("/pageerror")
+    
   }
 }
 
+const unblockProduct = async (req,res)=>{
+  try {
 
-
-
-
- 
-
+    let id = req.query.id;
+    await Product.updateOne({_id:id},{$set:{isBlocked:false}});
+    res.redirect("/admin/products");
+    
+  } catch (error) {
+    res.redirect("/pageerror")
+    
+  }
+}
 
 
 
@@ -179,5 +200,6 @@ module.exports = {
   getAllProducts,
   addProductOffer,
   removeProductOffer,
-
+  blockProduct,
+  unblockProduct,
 };
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -39,8 +39,11 @@ router.get("/deleteBrand",adminAuth,brandController.deleteBrand);
 router.get("/addProducts",adminAuth,productController.getProductAddPage);
 router.post("/addProducts",adminAuth,uploads.array("images",4),productController.addProducts);
 router.get("/products",adminAuth,productController.getAllProducts);
-router.post("/addProductOffer", adminAuth, productController.addProductOffer);
+router.post("/addProductOffer",adminAuth,productController.addProductOffer);
 router.post("/removeProductOffer",adminAuth,productController.removeProductOffer);
+router.get("/blockProduct",adminAuth,productController.blockProduct);
+router.get("/unblockProduct",adminAuth,productController.unblockProduct);
+
 
 
 
