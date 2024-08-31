22580216405f5a8b90f98ca68cb3c7cb68199560
Tony Kuriakose <t4tonykuriakose@gmail.com>
2024-08-31 23:39:56 +0530

    forgot

--- controllers/user/profileController.js
+++ controllers/user/profileController.js
@@ -1,49 +1,69 @@
 const User = require("../../models/userSchema");
 const nodemailer = require("nodemailer");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const env = require("dotenv").config();
 const session = require("express-session");
 
-// Forgot password management
-function generateOtp() {
+function generateOtp(){
   const digits = "1234567890";
   let otp = "";
-  for (let i = 0; i < 6; i++) {
-    otp += digits[Math.floor(Math.random() * 10)];
+  for(let i=0;i<6;i++){
+    otp+=digits[Math.floor(Math.random()*10)];
   }
   return otp;
 }
 
-// Function to send verification email
-const sendVerificationEmail = async (email, otp) => {
+const sendVerificationEmail = async (email,otp)=>{
   try {
+
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      port: 587,
-      secure: false,
-      requireTLS: true,
-      auth: {
-        user: process.env.NODEMAILER_EMAIL,
-        pass: process.env.NODEMAILER_PASSWORD,
-      },
-    });
+      service:"gmail",
+      port:587,
+      secure:false,
+      requireTLS:true,
+      auth:{
+        user:process.env.NODEMAILER_EMAIL,
+        pass:process.env.NODEMAILER_PASSWORD,
+
+      }
+
+    })
 
     const mailOptions = {
-      from: process.env.NODEMAILER_EMAIL,
-      to: email,
-      subject: "Your OTP for Password Reset",
-      text: `Your OTP is ${otp}`,
-      html: `<b><h4>Your OTP: ${otp}</h4><br></b>`,
-    };
+      from:process.env.NODEMAILER_EMAIL,
+      to:email,
+      subject:"Your OTP for password reset",
+      text:`Your OTP is ${otp}`,
+      html:`<b><h4>Your OTP: ${otp}</h4><br></b>`
+    }
 
     const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent: ", info.messageId);
+    console.log("Email sent:",info.messageId);
     return true;
+    
   } catch (error) {
-    console.error("Error sending email: ", error);
+
+    console.error("Error sending email",error);
     return false;
+    
   }
-};
+}
+
+const securePassword = async (password)=>{
+  try {
+
+    const passwordHash = await bcrypt.hash(password,10);
+    return passwordHash;
+    
+  } catch (error) {
+    
+  }
+}
+
+
+
+
+
 
 const getForgotPassPage = async (req, res) => {
   try {
@@ -53,119 +73,120 @@ const getForgotPassPage = async (req, res) => {
   }
 };
 
-const getForgotPassOtpPage = async (req, res) => {
+const forgotEmailValid = async (req,res)=>{
   try {
-    res.render("forgotPass-otp");
-  } catch (error) {
-    res.redirect("/pageNotFound");
-  }
-};
 
-const forgotEmailValid = async (req, res) => {
-  try {
-    const { email } = req.body;
-
-    const findUser = await User.findOne({ email: email });
-
-    if (findUser) {
+    const {email} = req.body;
+    const findUser = await User.findOne({email:email});
+    if(findUser){
       const otp = generateOtp();
-      const emailSent = await sendVerificationEmail(email, otp);
-      if (emailSent) {
+      const emailSent = await sendVerificationEmail(email,otp);
+      if(emailSent){
         req.session.userOtp = otp;
         req.session.email = email;
         res.render("forgotPass-otp");
-        console.log("OTP:", otp);
-      } else {
-        res.json({ success: false, message: "Failed to send OTP. Please try again." });
+        console.log("OTP:",otp);
+        
+      }else {
+        res.json({success:false, message:"Failed to send OTP. Please try again"});
       }
-    } else {
-      res.render("forgot-password", {
-        message: "User with this email does not exist",
+
+    }else {
+      res.render("forgot-password",{
+        message:"User with this email does not exist"
+
       });
     }
+    
   } catch (error) {
     res.redirect("/pageNotFound");
+    
   }
-};
+}
 
-const verifyForgotPassOtp = async (req, res) => {
+const verifyForgotPassOtp = async (req,res)=>{
   try {
+
     const enteredOtp = req.body.otp;
-    if (enteredOtp === req.session.userOtp) {
-      res.json({ success: true, redirectUrl: "/reset-password" });
-    } else {
-      res.json({ success: false, message: "OTP not matching" });
+    if(enteredOtp === req.session.userOtp){
+      res.json({success:true,redirectUrl:"/reset-password"});
+    }else {
+      res.json({success:false,message:"OTP not matching"});
     }
+    
   } catch (error) {
-    res.status(500).json({ success: false, message: "An error occurred. Please try again." });
+    res.status(500).json({success:false, message:"An error occured. Please try again"});
+    
   }
-};
+}
 
-const getResetPassPage = async (req, res) => {
+
+const getResetPassPage = async (req,res)=>{
   try {
+
     res.render("reset-password");
+    
   } catch (error) {
     res.redirect("/pageNotFound");
+    
   }
-};
+}
 
-const securePassword = async (password) => {
+const resendOtp = async (req,res)=>{
   try {
-    const passwordHash = await bcrypt.hash(password, 10);
-    return passwordHash;
-  } catch (error) {
-    console.error("Error in securePassword:", error);
-    throw new Error("failed to hash password");
-  }
-};
 
-const postNewPassword = async (req, res) => {
-  try {
-    const { newPass1, newPass2 } = req.body;
-    const email = req.session.email;
-    if (newPass1 === newPass2) {
-      const passwordHash = await securePassword(newPass1);
-      await User.updateOne(
-        { email: email },
-        {
-          $set: { password: passwordHash },
-        }
-      );
-      res.redirect("/login");
-    } else {
-      res.render("reset-password", { message: "Passwords do not match" });
-    }
-  } catch (error) {
-    res.redirect("/pageNotFound");
-  }
-};
-
-const resendOtp = async (req, res) => {
-  try {
     const otp = generateOtp();
     req.session.userOtp = otp;
     const email = req.session.email;
-
-    console.log("Resending OTP to email:", email);
-
-    const emailSent = await sendVerificationEmail(email, otp);
-    if (emailSent) {
-      console.log("Resent OTP:", otp);
-      res.status(200).json({ success: true, message: "Resend OTP successful" });
-    } else {
-      res.status(500).json({ success: false, message: "Failed to resend OTP" });
+    console.log("Resending OTP to email:",email);
+    const emailSent = await sendVerificationEmail(email,otp);
+    if(emailSent){
+      console.log("Resend OTP:",otp);
+      res.status(200).json({success:true, message: "Resend OTP Successful"});
+      
     }
+    
+    
   } catch (error) {
-    console.error("Error in resendOtp:", error);
-    res.status(500).json({ success: false, message: "Internal server error" });
+
+    console.error("Error in resend otp",error);
+    res.status(500).json({success:false , message:'Internal Server Error'});
+    
   }
-};
+}
+
+
+const postNewPassword = async (req,res)=>{
+  try {
+
+    const {newPass1, newPass2} = req.body;
+    const email = req.session.email;
+    if(newPass1===newPass2){
+      const passwordHash = await securePassword(newPass1);
+      await User.updateOne(
+        {email:email},
+        {$set:{password:passwordHash}}
+      )
+      res.redirect("/login");
+
+    }else {
+      res.render("reset-password",{message:'Passwords do not match'});
+    }
+    
+  } catch (error) {
+
+    res.redirect("/pageNotFound");
+    
+  }
+}
 
 module.exports = {
   getForgotPassPage,
   forgotEmailValid,
   verifyForgotPassOtp,
   getResetPassPage,
-  postNewPassword,
   resendOtp,
+  postNewPassword,
 };
+
+
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,43 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const userController = require('../controllers/user/userController');
+const userController = require("../controllers/user/userController");
 const profileController = require("../controllers/user/profileController");
 
-
 // Error Management
-router.get("/pageNotFound",userController.pageNotFound);
+router.get("/pageNotFound", userController.pageNotFound);
 
 // Sign up Management
-router.get("/signup",userController.loadSignup);
-router.post("/signup",userController.signup);
-router.post("/verify-otp",userController.verifyOtp);
-router.post("/resend-otp",userController.resendOtp);
-router.get('/auth/google',passport.authenticate('google',{scope:['profile','email']}));
-router.get('/auth/google/callback',passport.authenticate('google',{failureRedirect:'/signup'}),(req,res)=>{res.redirect('/')});
+router.get("/signup", userController.loadSignup);
+router.post("/signup", userController.signup);
+router.post("/verify-otp", userController.verifyOtp);
+router.post("/resend-otp", userController.resendOtp);
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/signup" }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
 
 // Login Management
-router.get("/login",userController.loadLogin);
-router.post("/login",userController.login);
+router.get("/login", userController.loadLogin);
+router.post("/login", userController.login);
 
 // Home page & Shopping page
-router.get('/',userController.loadHomepage);
-router.get("/logout",userController.logout);
+router.get("/", userController.loadHomepage);
+router.get("/logout", userController.logout);
 
 // Profile Management
-router.get("/forgot-password",profileController.getForgotPassPage);
+router.get("/forgot-password", profileController.getForgotPassPage);
 router.post("/forgot-email-valid", profileController.forgotEmailValid);
-router.post("/verify-PassForgot-otp", profileController.verifyForgotPassOtp);
+router.post("/verify-passForgot-otp",profileController.verifyForgotPassOtp);
+router.get("/reset-password",profileController.getResetPassPage);
 router.post("/resend-forgot-otp",profileController.resendOtp);
-router.get("/reset-password", profileController.getResetPassPage);
-router.post("/reset-password", profileController.postNewPassword);
-
-
-
-
-
-
-
-
+router.post("/reset-password",profileController.postNewPassword);
 
 module.exports = router;
