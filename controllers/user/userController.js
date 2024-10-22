d6edb43a6bbdf1b7b4781149bcabbb6097710f9c
Tony Kuriakose <t4tonykuriakose@gmail.com>
2024-10-22 19:16:10 +0530

    banner in home page

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,6 +1,7 @@
 const User = require("../../models/userSchema");
 const Category = require("../../models/categorySchema");
 const Product = require("../../models/productSchema");
+const Banner = require("../../models/bannerSchema");
 const env = require("dotenv").config();
 const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
@@ -15,6 +16,11 @@ const pageNotFound = async (req, res) => {
 
 const loadHomepage = async (req, res) => {
   try {
+    const today = new Date().toISOString();
+    const findBanner = await Banner.find({
+      startDate:{$lt:new Date(today)},
+      endDate:{$gt:new Date(today)},
+    })
     const user = req.session.user;
     const categories = await Category.find({isListed:true});
     let productData = await Product.find(
@@ -28,7 +34,7 @@ const loadHomepage = async (req, res) => {
 
     if (user) {
       const userData = await User.findOne({ _id: user._id });
-      return res.render("home", { user: userData ,products:productData});
+      return res.render("home", { user: userData ,products:productData,banner:findBanner || []});
     } else {
       return res.render("home",{products:productData,banner:findBanner || []});
     }
